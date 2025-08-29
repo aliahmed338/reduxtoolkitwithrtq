f0d1b41899301fb6c1a6564684f3b9bb917cd3d2
aliahmed338 <aliahmed338@gmail.com>
2025-08-29 22:26:31 +0300

    add productsApi with rtq and use it in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "./store/features/counterSlice";
+import { useGetAllProductsQuery } from "./store/api/productsApi";
 
 const App = () => {
   const counter = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  useNav;
+  const { data, isLoading, error, isError } = useGetAllProductsQuery();
+  console.log(data?.products);
   return (
     <div>
       <p>{counter}</p>
       <button onClick={() => dispatch(increment())}>increment</button>
       <button onClick={() => dispatch(decrement())}>decrement</button>
+
+      <div>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Error: {error.error}</p>}
+        {data && data.products.map((p) => <div key={p.id}>{p.title}</div>)}
+      </div>
     </div>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice.js";
+import { productApi } from "./api/productsApi.js";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([productApi.middleware]),
 });
-
-
